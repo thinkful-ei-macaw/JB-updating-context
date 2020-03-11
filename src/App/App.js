6e459326f17bfd697a6fea78c03e2985be500451
Javori Smart <jsmart1993@hotmail.com>
2020-03-11 11:25:29 -0700

    finished context refactoring

--- src/App/App.js
+++ src/App/App.js
@@ -75,11 +75,7 @@ class App extends Component {
                 <Route
                     path="/note/:noteId"
                     component={NotePageMain}
-                // render={routeProps => {
-                //     const { noteId } = routeProps.match.params;
-                //     const note = findNote(notes, noteId);
-                //     return <NotePageMain {...routeProps} note={note} />;
-                // }}
+        
                 />
             </>
         );
